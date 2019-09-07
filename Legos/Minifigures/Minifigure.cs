4ec31533c05c626852622acd56a3c6353c4da6c8
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-09-07 10:28:27 -0500

    torso and battles

--- Legos/Minifigures/Minifigure.cs
+++ Legos/Minifigures/Minifigure.cs
@@ -4,6 +4,21 @@ namespace Legos.Minifigures
 {
     class Minifigure
     {
-        public Head Head {get;  set; }
+        public Head Head { get; }
+
+        public Torso Torso { get; }
+
+        public Minifigure(Head head, Torso torso)
+        {
+            Head = head;
+            Torso = torso;
+        }
+        
+        public void Battle()
+        {
+            Head.Talk();
+            Torso.Flex();
+            Torso.Fight();
+        }
     }
 }
